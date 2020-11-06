d7c588998a5eec1351326c78d6b327b28c60bfaf
Олег Бавыкин <invisible6462@gmail.com>
2020-11-06 18:08:42 +0000

    Добавил сущность Event

---
+++ Assets/Scripts/Entity/Event.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Event
+{
+    public int id;
+    public int idNextStage;
+    public List<string> types;
+    public int turns;
+    public string title;
+    public string description;
+    public List<string> optionText;
+    public List<Result> resultList;
+    public List<string> effectsText;
+}
+
+
+[System.Serializable]
+public class Result
+{
+    public Resource resource;
+    public int deltaValue;
+}
+
+public enum Resource
+{
+    ENERGY,
+    OXYGEN,
+    FOOD,
+    PEOPLE,
+    MORALE,
+    REACTOR,
+    WATER
+}
