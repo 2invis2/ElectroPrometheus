9e9ef73e4537972c1c245d101c73faf94e1e4580
Dry <dry99rus@gmail.com>
2020-11-07 19:59:49 +0000

    hotfix

--- Assets/Scripts/Director.cs
+++ Assets/Scripts/Director.cs
@@ -7,6 +7,7 @@ public class Director : MonoBehaviour
     public GameObject[] rooms = new GameObject[12];
     public GameObject eventManagerOBJ;
     public GameObject resourceManagerOBJ;
+	public int eventLimit = 6;
     private ResourceManager resourceManager;
     private EventManager eventManager;
 
@@ -24,17 +25,30 @@ public class Director : MonoBehaviour
         {
             room.GetComponent<Room>().UpdateEvent();			
         }
-		int random1 = Random.Range(0, (freeRooms().Count-1));
-		int random2 = Random.Range(0, (freeRooms().Count-1));
-		while (random1 == random2)
+		
+		List<int> freeRooms = FreeRooms();
+		if (freeRooms.Count>(13-eventLimit))
 		{
-			random2 = Random.Range(0, (freeRooms().Count-1));
+			int random1 = Random.Range(0, (freeRooms.Count-1));
+			int random2 = Random.Range(0, (freeRooms.Count-1));
+			while (random1 == random2)
+			{
+				random2 = Random.Range(0, (freeRooms.Count-1));
+			}
+			int randomFreeRoom1 = freeRooms[random1];
+			int randomFreeRoom2 = freeRooms[random2];
+			Debug.Log(" спаун в комнатах - " + randomFreeRoom1 + " " + randomFreeRoom2);
+			CreateEventByType(TypeEvent.GREEN, rooms[randomFreeRoom1]);
+			CreateEventByType(TypeEvent.GREEN, rooms[randomFreeRoom2]);
 		}
-		int randomFreeRoom1 = freeRooms()[random1];
-		int randomFreeRoom2 = freeRooms()[random2];
-		Debug.Log(" спаун в комнатах - " + randomFreeRoom1 + " " + randomFreeRoom2);
-		CreateEventByType(TypeEvent.GREEN, rooms[randomFreeRoom1]);
-		CreateEventByType(TypeEvent.GREEN, rooms[randomFreeRoom2]);
+		else
+			if(freeRooms.Count == 13-eventLimit)
+			{
+				int random1 = Random.Range(0, (freeRooms.Count-1));
+				int randomFreeRoom1 = freeRooms[random1];
+				Debug.Log(" спаун в комнате - " + randomFreeRoom1);
+				CreateEventByType(TypeEvent.GREEN, rooms[randomFreeRoom1]);
+			}
 		
 
     }
@@ -44,7 +58,7 @@ public class Director : MonoBehaviour
         eventManager.initEventByTag(type.ToString(), roomTo);
     }
 	
-	private List<int> freeRooms()
+	private List<int> FreeRooms()
 	{
 		List<int> ans = new List<int>();
 		for	(int i=0; i<12; i++)
